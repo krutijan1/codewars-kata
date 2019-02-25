dfd57fb82e660edcf48d63a1680854391e83e7b1
krutijan1 <krutinajan@gmail.com>
2019-02-25 16:47:12 +0100

    array inversion count

---
+++ data_structures_and_algorhitms/calculate_number_of_inversion_in_array/solution.js
@@ -0,0 +1,23 @@
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function countInversions(arr) {
+  let inversions = 0;
+  let len = arr.length;
+  let stop = 0;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0, stop = len - i; j < stop; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        // number of inversions is number of swaps needed
+        inversions += 1;
+      }
+    }
+  }
+
+  return inversions;
+}
