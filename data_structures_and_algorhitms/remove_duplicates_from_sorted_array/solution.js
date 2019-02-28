e71716183134080568844c8690598aa811d086bf
krutijan1 <krutinajan@gmail.com>
2019-02-28 13:07:27 +0100

    add duplicates

---
+++ data_structures_and_algorhitms/remove_duplicates_from_sorted_array/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+};
