db275e2d293d074300611a6b2188be4d36dbad49
krutijan1 <krutinajan@gmail.com>
2019-03-07 14:20:39 +0100

    add solution

---
+++ freecode_camp_data_structures_and_algorhitms/perform_a_two_union_set/solution.js
@@ -0,0 +1,46 @@
+function Set() {
+  const collection = [];
+
+  this.has = function(element) {
+    return collection.indexOf(element) !== -1;
+  };
+
+  this.values = function() {
+    return collection;
+  };
+
+  this.add = function(element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+
+  this.remove = function(element) {
+    if (this.has(element)) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  this.size = function() {
+    return collection.length;
+  };
+
+  this.union = function(coll) {
+    const resultSet = new Set();
+
+    for (const value of this.values()) {
+      resultSet.add(value);
+    }
+
+    for (const value of coll.values()) {
+      resultSet.add(value);
+    }
+
+    return resultSet;
+  };
+}
