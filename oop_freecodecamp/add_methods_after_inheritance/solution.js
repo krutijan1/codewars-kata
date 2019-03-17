227ff94c283e09b3e007a4575f329a99876b30bb
krutijan1 <krutinajan@gmail.com>
2019-03-17 11:23:26 +0100

    add solution

---
+++ oop_freecodecamp/add_methods_after_inheritance/solution.js
@@ -0,0 +1,19 @@
+function Animal() {}
+Animal.prototype.eat = function() {
+  console.log('nom nom nom');
+};
+
+function Dog() {}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+  console.log('Wau Wau!');
+};
+
+let beagle = new Dog();
+
+beagle.eat(); // Should print "nom nom nom"
+beagle.bark(); // Should print "Woof!"
