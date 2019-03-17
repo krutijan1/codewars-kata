1c230fba2892e2a1878c907a9f192c1723829051
krutijan1 <krutinajan@gmail.com>
2019-03-17 20:17:22 +0100

    add solution

---
+++ basic_javascript/write_reusable_functions_with_javascript/solution.js
@@ -0,0 +1,12 @@
+// Example
+function ourReusableFunction() {
+  console.log('Heyya, World');
+}
+
+ourReusableFunction();
+
+function reusableFunction() {
+  console.log('Hi World');
+}
+
+reusableFunction();
