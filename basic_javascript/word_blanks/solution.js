b81f28a632bdc71df1bcc815244057e9838e861d
krutijan1 <krutinajan@gmail.com>
2019-03-17 19:33:40 +0100

    add solution

---
+++ basic_javascript/word_blanks/solution.js
@@ -0,0 +1,9 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = 'The ' + myAdjective + ' ' + myNoun + ' ' + myVerb + ' ' + myAdverb + '.';
+
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks('dog', 'big', 'ran', 'quickly');
