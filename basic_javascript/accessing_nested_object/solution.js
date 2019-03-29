6d491c935bafc7b06c12dcdfb2d4cc2b3a82d181
krutijan1 <krutinajan@gmail.com>
2019-03-29 13:03:04 +0100

    add solution

---
+++ basic_javascript/accessing_nested_object/solution.js
@@ -0,0 +1,14 @@
+// Setup
+let myStorage = {
+  car: {
+    inside: {
+      'glove box': 'maps',
+      'passenger seat': 'crumbs'
+    },
+    outside: {
+      trunk: 'jack'
+    }
+  }
+};
+
+let gloveBoxContents = myStorage.car.inside['glove box']; // Change this line
