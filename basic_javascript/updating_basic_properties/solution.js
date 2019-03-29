9b327aec4cebef1058f54d52b691f816e2aef9e3
krutijan1 <krutinajan@gmail.com>
2019-03-29 12:20:23 +0100

    add solution

---
+++ basic_javascript/updating_basic_properties/solution.js
@@ -0,0 +1,21 @@
+// Example
+let ourDog = {
+  name: 'Camper',
+  legs: 4,
+  tails: 1,
+  friends: ['everything!']
+};
+
+ourDog.name = 'Happy Camper';
+
+// Setup
+let myDog = {
+  name: 'Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers']
+};
+
+// Only change code below this line.
+
+myDog['name'] = 'Happy coder';
