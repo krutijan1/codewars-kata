cf4cc82f957a76612380f7b92d2a4c489922ee3e
krutijan1 <krutinajan@gmail.com>
2019-03-29 13:05:10 +0100

    add solution

---
+++ basic_javascript/accessing_nested_array/solution.js
@@ -0,0 +1,15 @@
+// Setup
+let myPlants = [
+  {
+    type: 'flowers',
+    list: ['rose', 'tulip', 'dandelion']
+  },
+  {
+    type: 'trees',
+    list: ['fir', 'pine', 'birch']
+  }
+];
+
+// Only change code below this line
+
+let secondTree = myPlants[1].list[1]; // Change this line
